1df791b637a43bc82259247e29b9d4a6c664177e
sh_park <thdgml6017@naver.com>
2018-12-10 18:30:15 +0900

    commit

--- nomadinstargram/config/settings/base.py
+++ nomadinstargram/config/settings/base.py
@@ -39,7 +39,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres://localhost/nomadinstargram'),
+    'default': env.db('DATABASE_URL', default='postgres://postgres@localhost:5432/nomad_instargram'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
@@ -63,15 +63,16 @@ DJANGO_APPS = [
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
-    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'rest_framework',
 ]
+# 사용자가 생성한
 LOCAL_APPS = [
     'nomadinstargram.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
+    'nomadinstargram.images.apps.ImagesConfig'
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
